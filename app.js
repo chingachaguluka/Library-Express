040a75044205af00b69f52ec158a4e05272ac685
Chinga <royalse-me@yahoo.com>
2017-04-11 19:08:17 +0200

    Move js css img files under views to resolve failure to load dependencies

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 //gets a response on the particular path - home and render a template
-app.get('/', function(req,res){
+app.get('/', function(req,res) {
     res.render('index', {title: 'Welcome to Home in EJS', list: ['a', 'b', 'c']});
 });
 
 //gets a response on the particular path - home and return a message in the send to
 //the browser
-app.get('/books', function(req,res){
+app.get('/books', function(req,res) {
     res.send('Welcome to books section');
 });
 
 //express listens on the port parameter and the callback determines what it performs
-app.listen(port, function(err){
+app.listen(port, function(err) {
     console.log('Server listening on port ' + port);
 });
