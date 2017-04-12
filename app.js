598086d3b8d388b677f0ff891b556ac1f7e49b47
Chinga <royalse-me@yahoo.com>
2017-04-12 18:19:55 +0200

    Complete book view  routing and displaying

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
+var bookRouter = require('./src/routes/book-router');
 
 var app = express();
 
 var port = process.env.PORT || 5000;
-var bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
@@ -12,58 +12,14 @@ app.set('view engine', 'ejs');
 
 //gets a response on the particular path - home and render a template
 app.get('/', function(req,res) {
-    //res.render('index', {bookId: '4', title: 'The New Express Developer', price: '345.66', author: 'Chinga Chaguluka', genre: 'Technology'});
     res.render('index', {nav: [
             {Link: '/Books', Title:'Books'},
             {Link: '/Authors', Title: 'Authors'}]
     });
 });
 
-var books = [
-    {
-        id: '1',
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: 'False'
-    },
-    {
-        id: '2',
-        title: 'Les Miserables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: 'False'
-    },
-    {
-        id: '3',
-        title: 'The Time Machine',
-        genre: 'Science Fiction',
-        author: 'H. G. Wells',
-        read: 'False'
-    }
-];
-
-bookRouter.route('/')
-    .get(function(req, res) {
-        res.render('book-list',  {nav: [
-            {Link: '/Books', Title:'Books'},
-            {Link: '/Authors', Title: 'Authors'}],
-             books: books
-        });
-    });
-
-bookRouter.route('/Single')
-    .get(function(req, res) {
-        res.send('This is the book view page');
-    });
-
+//gets a response on books patch and uses the bookRouter module to handle the routing
 app.use('/Books', bookRouter);
-
-//gets a response on the particular path - home and return a message in the send to
-//the browser
-app.get('/books', function(req,res) {
-    res.send('Welcome to books section');
-});
 
 //express listens on the port parameter and the callback determines what it performs
 app.listen(port, function(err) {
--- src/routes/book-router.js
+++ src/routes/book-router.js
@@ -2,3 +2,47 @@ var express = require('express');
 
 var bookRouter = express.Router();
 
+var books = [
+    {
+        id: '1',
+        title: 'War and Peace',
+        genre: 'Historical Fiction',
+        author: 'Lev Nikolayevich Tolstoy',
+        read: 'False'
+    },
+    {
+        id: '2',
+        title: 'Les Miserables',
+        genre: 'Historical Fiction',
+        author: 'Victor Hugo',
+        read: 'False'
+    },
+    {
+        id: '3',
+        title: 'The Time Machine',
+        genre: 'Science Fiction',
+        author: 'H. G. Wells',
+        read: 'False'
+    }
+];
+
+bookRouter.route('/')
+    .get(function(req, res) {
+        res.render('book-list',  {nav: [
+            {Link: '/Books', Title:'Books'},
+            {Link: '/Authors', Title: 'Authors'}],
+                books: books
+            });
+    });
+
+bookRouter.route('/:id')
+    .get(function(req, res) {
+        var id = req.params.id;
+        res.render('book-view',  {nav: [
+            {Link: '/Books', Title:'Books'},
+            {Link: '/Authors', Title: 'Authors'}],
+                book: books[id]
+            });
+    });
+
+module.exports = bookRouter;
