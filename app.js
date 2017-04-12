2bc7678b20d687731fcd22556fbad20b9d79181a
Chinga <royalse-me@yahoo.com>
2017-04-12 13:04:41 +0200

    Change index page and create routes to books module

--- app.js
+++ app.js
@@ -13,16 +13,43 @@ app.set('view engine', 'ejs');
 //gets a response on the particular path - home and render a template
 app.get('/', function(req,res) {
     //res.render('index', {bookId: '4', title: 'The New Express Developer', price: '345.66', author: 'Chinga Chaguluka', genre: 'Technology'});
-    res.render('index', {nav: [{
-            Link: '/Authors', Title: 'Authors'
-        },
-            {Link: '/Books', Title:'Books'}]
+    res.render('index', {nav: [
+            {Link: '/Books', Title:'Books'},
+            {Link: '/Authors', Title: 'Authors'}]
     });
 });
 
+var books = [
+    {
+        id: '1',
+        title: 'War and Peace',
+        genre: 'Historical Fiction',
+        author: 'Lev Nikolayevich Tolstoy',
+        read: 'False'
+    },
+    {
+        id: '2',
+        title: 'Les Miserables',
+        genre: 'Historical Fiction',
+        author: 'Victor Hugo',
+        read: 'False'
+    },
+    {
+        id: '3',
+        title: 'The Time Machine',
+        genre: 'Science Fiction',
+        author: 'H. G. Wells',
+        read: 'False'
+    }
+];
+
 bookRouter.route('/')
     .get(function(req, res) {
-        res.send('This is the books listing');
+        res.render('book-list',  {nav: [
+            {Link: '/Books', Title:'Books'},
+            {Link: '/Authors', Title: 'Authors'}],
+             books: books
+        });
     });
 
 bookRouter.route('/Single')
