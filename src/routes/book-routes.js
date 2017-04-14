ec2783d4961b7e712551193af11caf07e315a993
chingachaguluka <chinga.e.chaguluka@gmail.com>
2017-04-14 23:22:57 +0200

    Retrieving list of books from mongodb completed

--- src/routes/book-routes.js
+++ src/routes/book-routes.js
@@ -1,37 +1,22 @@
 var express = require('express');
+var mongodb = require('mongodb').MongoClient;
 
 var bookRouter = express.Router();
 
-//books in the library
-var books = [
-    {
-        id: '1',
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: 'False'
-    },
-    {
-        id: '2',
-        title: 'Les Miserables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: 'False'
-    },
-    {
-        id: '3',
-        title: 'The Time Machine',
-        genre: 'Science Fiction',
-        author: 'H. G. Wells',
-        read: 'False'
-    }
-];
 
 //exported function to manage routes for the books
 var router = function(nav) {
     bookRouter.route('/')
         .get(function(req, res) {
-            res.render('book-list',  {nav: nav, books: books});
+
+            var url = 'mongodb://localhost:27017/Library';
+            mongodb.connect(url, function(err, db) {
+                var collection = db.collection('Books');
+                collection.find({}).toArray(function(err, results) {
+                    console.log(results);
+                    res.render('book-list',  {nav: nav, books: results});
+                });
+            });
         });
 
     bookRouter.route('/:id')
