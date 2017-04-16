d8201dfd324f53da09f60d7228ea1690f3980ebd
chingachaguluka <chinga.e.chaguluka@gmail.com>
2017-04-16 16:11:12 +0200

    Create a login page, install body-parser create auth route to manage signup

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 
 var app = express();
 
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 var nav = [
             {Link: '/Books', Title:'Books'},
@@ -13,12 +15,15 @@ var nav = [
             {Link: '/Admin', Title: 'Admin'}
         ];
 
+//setting up sourse of the views in the project
+app.set('views', './src/views');
+//setting up the templating engine of the project
+app.set('view engine', 'ejs');
+
+//importing routers from the routers files
 var adminRouter = require('./src/routes/admin-routes')(nav);
 var bookRouter = require('./src/routes/book-routes')(nav);
-
-app.set('views', './src/views');
-
-app.set('view engine', 'ejs');
+var authRouter = require('./src/routes/auth-routes')(nav);
 
 //gets a response on the particular path - home and render a template
 app.get('/', function(req,res) {
@@ -30,6 +35,9 @@ app.use('/Books', bookRouter);
 
 //uses admin-routes to manage routes to admin
 app.use('/admin', adminRouter);
+
+//users auth-routes to manage routing to auth
+app.use('/auth', authRouter);
 
 //express listens on the port parameter and the callback determines what it performs
 app.listen(port, function(err) {
